23e43fcb7361601f49c4d89a85584ccd8f60823d
Lance Ye <lance_yjc@outlook.com>
2019-10-03 01:00:20 +0800

    shot

--- base/clear_shot.py
+++ base/clear_shot.py
@@ -3,10 +3,13 @@ import os
 
 def clear_shot():
     # path = os.path.split(os.getcwd())[0] + '\\screenshot'
-    path = os.getcwd() + '\\screenshot'
-    for file_name in os.listdir(path):
-        if file_name.endswith('.png'):
-            os.remove(path + '\\' + file_name)
+    try:
+        path = os.getcwd() + '\\screenshot'
+        for file_name in os.listdir(path):
+            if file_name.endswith('.png'):
+                os.remove(path + '\\' + file_name)
+    except Exception:
+        pass
 
 
 if __name__ == '__main__':
