0d8947009658703ba5b67a1f5e2bfa66c88ef917
Lance Ye <lance_yjc@outlook.com>
2019-10-03 20:19:21 +0800

    app6.0

--- testcases/__init__.py
+++ testcases/__init__.py
@@ -3,5 +3,15 @@ sys.path.append(os.getcwd())
 
 from base.clear_shot import clear_shot
 from base.base_driver import env_config
+
+
+def make_screenshot_dir():
+    screenshot_path = os.getcwd() + '\\screenshot'
+    if not os.path.exists(screenshot_path):
+        os.mkdir(screenshot_path)
+    else:
+        clear_shot()
+
+
 env_config()
-clear_shot()
+make_screenshot_dir()
